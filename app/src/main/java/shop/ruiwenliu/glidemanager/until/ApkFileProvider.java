c6621af7420a489e3f3d7aee2fc52ed6e44210ed
Liuruiwen <1054750389@qq.com>
2018-12-19 11:59:18 +0800

    更新了下载相关类

--- app/src/main/java/shop/ruiwenliu/glidemanager/until/ApkFileProvider.java
+++ app/src/main/java/shop/ruiwenliu/glidemanager/until/ApkFileProvider.java
@@ -3,9 +3,9 @@ package shop.ruiwenliu.glidemanager.until;
 import android.support.v4.content.FileProvider;
 
 /**
- * Auther   : BinJian
- * Date     : 2017/10/19 0019
- * Desc     :
+ Created by Amuse
+ * Data:2018/11/27 0027
+ * Desc:
  */
 
 public class ApkFileProvider extends FileProvider {
